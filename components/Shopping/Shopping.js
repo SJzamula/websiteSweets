32418d96348834f013923f0346a4db2543ed5f61
sanixto <sanix.to@gmail.com>
2021-05-18 23:18:15 +0300

    fix Shopping

--- components/Shopping/Shopping.js
+++ components/Shopping/Shopping.js
@@ -1,39 +1,38 @@
 class Shopping {
-handleClear() {
+  handleClear() {
     ROOT_SHOPPING.innerHTML = '';
-}
+  }
 
-    render() {
-        const pruductsStore = localStorageUtil.getProducts();
-        let htmlCatalog = '';
-        let sumCatalog = 0;
+  render() {
+    const pruductsStore = localStorageUtil.getProducts();
+    let htmlCatalog = '';
+    let sumCatalog = 0;
 
-        CATALOG.forEach(({id, name, price}) => {
-            if (productsStore.indexOf(id) !== -1) {
-                htmlCatalog += `
-                    <tr>
-                        <td class="shopping-element__name">${name}</td>
-                        <td class="shopping-element__price">${price.toLocaleString() USD}</td>
-                    </tr>
-                `;
-            }
-        });
-
-        const html =`
-            <div class="shopping-container">
-                <div class="shopping__close" onclick="shoppingPage.handleClear"></div>
-                <table>
-                    ${htmlCatalog}
-                    <tr>
-                        <td class="shopping-element__name">Сумма</td>
-                        <td class="shopping-element__price">${sumCatalog.toLocaleString() USD}</td>
-                    </tr>
-                </table>
-            </div>
+    CATALOG.forEach(({ id, name, price }) => {
+      if (productsStore.indexOf(id) !== -1) {
+        htmlCatalog += `
+          <tr>
+            <td class="shopping-element__name">${name}</td>
+            <td class="shopping-element__price">${price.toLocaleString()} USD</td>
+          </tr>
         `;
-        ROOT_SHOPPING.innerHTML = html;
+      }
+    });
 
-    }
+    const html = `
+      <div class="shopping-container">
+        <div class="shopping__close" onclick="shoppingPage.handleClear"></div>
+        <table>
+          ${htmlCatalog}
+          <tr>
+            <td class="shopping-element__name">Сумма</td>
+            <td class="shopping-element__price">${sumCatalog.toLocaleString()} USD</td>
+          </tr>
+        </table>
+      </div>
+    `;
+    ROOT_SHOPPING.innerHTML = html;
+  }
 }
 
 const shoppingPage = new Shopping();
